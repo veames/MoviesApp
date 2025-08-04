c270846660b451d75284bf93448990d63ff5ebf6
Pavel <paulsema52@gmail.com>
2025-08-04 21:43:21 +0700

    Created POJO

---
+++ app/src/main/java/com/veames/movies/MovieResponse.java
@@ -0,0 +1,20 @@
+package com.veames.movies;
+
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+public class MovieResponse {
+
+    @SerializedName("docs")
+    private List<Movie> movies;
+
+    public MovieResponse(List<Movie> movies) {
+        this.movies = movies;
+    }
+
+    public List<Movie> getMovies() {
+        return movies;
+    }
+
+}
---
+++ app/src/main/java/com/veames/movies/Rating.java
@@ -0,0 +1,18 @@
+package com.veames.movies;
+
+import com.google.gson.annotations.SerializedName;
+
+public class Rating {
+
+    @SerializedName("kp")
+    private String kp;
+
+    public Rating(String kp) {
+        this.kp = kp;
+    }
+
+    public String getKp() {
+        return kp;
+    }
+
+}
