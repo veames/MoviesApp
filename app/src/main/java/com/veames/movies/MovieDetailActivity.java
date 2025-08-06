180bd38cfd98abcb805152a9882eee163903b36a
Pavel <paulsema52@gmail.com>
2025-08-06 21:04:02 +0700

    Created layout for MovieDetailActivity

---
+++ app/src/main/java/com/veames/movies/MovieDetailActivity.java
@@ -0,0 +1,40 @@
+package com.veames.movies;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MovieDetailActivity extends AppCompatActivity {
+
+    private ImageView imageViewPoster;
+    private TextView textViewTitle;
+    private TextView textViewYear;
+    private TextView textViewDescription;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_movie_detail);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        initViews();
+    }
+
+    private void initViews() {
+        imageViewPoster = findViewById(R.id.imageViewPoster);
+        textViewTitle = findViewById(R.id.textViewTitle);
+        textViewYear = findViewById(R.id.textViewYear);
+        textViewDescription = findViewById(R.id.textViewDescription);
+    }
+
+}
