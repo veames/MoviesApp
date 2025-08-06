ef49579e9f590f83bf5a87cbf4b0035b3dede30e
Pavel <paulsema52@gmail.com>
2025-08-06 21:41:14 +0700

    Added implementation of Serializable interface for Movie, Rating and Poster classes

--- app/src/main/java/com/veames/movies/Movie.java
+++ app/src/main/java/com/veames/movies/Movie.java
@@ -4,7 +4,9 @@ import com.google.gson.annotations.SerializedName;
 
 import org.json.JSONException;
 
-public class Movie {
+import java.io.Serializable;
+
+public class Movie implements Serializable {
 
     @SerializedName("id")
     private int id;
