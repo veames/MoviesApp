2e20c23e022e98296273d449bedc9fc908e1c5f3
Pavel <paulsema52@gmail.com>
2025-08-11 21:37:55 +0700

    Added MoviesAdapter for RecyclerView. Changed CardView in root element

--- app/src/main/java/com/veames/movies/MainActivity.java
+++ app/src/main/java/com/veames/movies/MainActivity.java
@@ -10,6 +10,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.List;
 
@@ -24,6 +26,9 @@ public class MainActivity extends AppCompatActivity {
 
     private MainViewModel viewModel;
 
+    private RecyclerView recyclerViewMovies;
+    private MoviesAdapter moviesAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,11 +40,17 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
 
+        recyclerViewMovies = findViewById(R.id.recyclerViewMovies);
+        moviesAdapter = new MoviesAdapter();
+        recyclerViewMovies.setAdapter(moviesAdapter);
+        recyclerViewMovies.setLayoutManager(new GridLayoutManager(this, 2));
+
         viewModel = new ViewModelProvider(this).get(MainViewModel.class);
         viewModel.getMovies().observe(this, new Observer<List<Movie>>() {
             @Override
             public void onChanged(List<Movie> movies) {
-                Log.d(TAG, movies.toString());
+//                Log.d(TAG, movies.toString());
+                moviesAdapter.setMovies(movies);
             }
         });
         viewModel.loadMovies();
---
+++ app/src/main/java/com/veames/movies/MoviesAdapter.java
@@ -0,0 +1,65 @@
+package com.veames.movies;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MoviesAdapter extends RecyclerView.Adapter<MoviesAdapter.MovieViewHolder> {
+
+    private List<Movie> movies = new ArrayList<>();
+
+    public void setMovies(List<Movie> movies) {
+        this.movies = movies;
+        notifyDataSetChanged();
+    }
+
+    @NonNull
+    @Override
+    public MovieViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(
+                R.layout.movie_item,
+                parent,
+                false
+        );
+        return new MovieViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MovieViewHolder holder, int position) {
+        Movie movie = movies.get(position);
+        Glide.with(holder.itemView)
+                .load(movie.getPoster().getUrl())
+                .into(holder.imageViewPoster);
+        double rating = movie.getRating().getRatingKp();
+        holder.textViewRating.setText(String.format("%.1f", rating));
+    }
+
+    @Override
+    public int getItemCount() {
+        return movies.size();
+    }
+
+    static class MovieViewHolder extends RecyclerView.ViewHolder {
+
+        private final ImageView imageViewPoster;
+        private final TextView textViewRating;
+
+        public MovieViewHolder(@NonNull View itemView) {
+            super(itemView);
+            imageViewPoster = itemView.findViewById(R.id.imageViewPoster);
+            textViewRating = itemView.findViewById(R.id.textViewRating);
+        }
+
+    }
+
+}
